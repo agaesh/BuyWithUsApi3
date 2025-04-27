1dc91f435dd076b380b6f816ff336a98d9a92d6b
agaesh <agaeshk@gmail.com>
2025-04-27 16:15:27 +0800

    Fix route import for ProductRoutes in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const db = require('./db'); // Assuming db.js is in the same directory
 const app = express();
 const PORT = 3000;
 const userRoute = require('./routes/userRoutes'); // Assuming userRoutes.js is in the same directory
-
+const productRoute = require('./routes/ProductRoutes'); // Assuming ProductRoutes.js is in the same directory
 // Middleware to parse JSON
 app.use(express.json());
 // Check database connection
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/user', userRoute);
-
+app.use('/products', productRoute);
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT} `);
